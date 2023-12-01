4fd0b51f6a539ca55781bdaf69c7eba56386fd5b
Angel Lakra <angel.lakra@chainlabs.in>
2023-12-01 23:55:36 +0530

    added sentry for webhook

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { createYoga } from "graphql-yoga";
 import express from "express";
 import bodyParser from "body-parser";
@@ -54,26 +55,26 @@ app.post("/api/webhook", async (req, res) => {
   if (req.method === "POST") {
     console.log({ req: req.body });
     const order_url = req.body.api_url;
-    Sentry.captureMessage("WEbhook REceived");
+    // Sentry.captureMessage("WEbhook REceived");
 
-    // const order = await axios.get(order_url, {
-    //   headers: {
-    //     Authorization: `Bearer ${process.env.NEXT_PUBLIC_EVENTBRITE_OAUTH}`,
-    //   },
-    // });
+    const order = await axios.get(order_url, {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_EVENTBRITE_OAUTH}`,
+      },
+    });
 
-    // console.log({ order });
-    // Sentry.captureMessage(
-    //   `Order Placed by ${order.data.first_name} ${
-    //     order.data.last_name
-    //   } with email ${order.data.email} at ${new Date(order.data.created)}.`,
-    //   {
-    //     tags: {
-    //       body: req.body,
-    //       order: order.data,
-    //     },
-    //   }
-    // );
+    console.log({ order });
+    Sentry.captureMessage(
+      `Order Placed by ${order.data.first_name} ${
+        order.data.last_name
+      } with email ${order.data.email} at ${new Date(order.data.created)}.`,
+      {
+        tags: {
+          body: req.body,
+          order: order.data,
+        },
+      }
+    );
 
     // Perform actions (e.g., create a user) here
     res.status(200).json({ message: "User created successfully" });

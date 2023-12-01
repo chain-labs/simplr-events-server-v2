54f82da395f400a241805ab1888a7c943eb13fa3
Angel Lakra <angel.lakra@chainlabs.in>
2023-12-01 18:59:25 +0530

    removed func in webhook

--- src/app.js
+++ src/app.js
@@ -57,24 +57,24 @@ app.post("/api/webhook", async (req, res) => {
       console.log({ req: req.body });
       const order_url = req.body.api_url;
 
-      const order = await axios.get(order_url, {
-        headers: {
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_EVENTBRITE_OAUTH}`,
-        },
-      });
+      // const order = await axios.get(order_url, {
+      //   headers: {
+      //     Authorization: `Bearer ${process.env.NEXT_PUBLIC_EVENTBRITE_OAUTH}`,
+      //   },
+      // });
 
-      console.log({ order });
-      Sentry.captureMessage(
-        `Order Placed by ${order.data.first_name} ${
-          order.data.last_name
-        } with email ${order.data.email} at ${new Date(order.data.created)}.`,
-        {
-          tags: {
-            body: req.body,
-            order: order.data,
-          },
-        }
-      );
+      // console.log({ order });
+      // Sentry.captureMessage(
+      //   `Order Placed by ${order.data.first_name} ${
+      //     order.data.last_name
+      //   } with email ${order.data.email} at ${new Date(order.data.created)}.`,
+      //   {
+      //     tags: {
+      //       body: req.body,
+      //       order: order.data,
+      //     },
+      //   }
+      // );
 
       // Perform actions (e.g., create a user) here
       res.status(200).json({ message: "User created successfully" });

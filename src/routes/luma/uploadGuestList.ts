f1a6bba17132989ff494807f65957d2f9704ab04
Angel Lakra <angel.lakra@chainlabs.in>
2024-01-13 13:01:08 +0530

    upload file on luma fix for repititive uploads and emails sent

--- src/routes/luma/uploadGuestList.ts
+++ src/routes/luma/uploadGuestList.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { log } from "../../utils/logger.utils.js";
+import { log, logError } from "../../utils/logger.utils.js";
 import writeHoldersToBatch from "../../functions/writeHoldersToBatch.js";
 import addBatch from "../../functions/addBatch.js";
 
@@ -23,64 +23,70 @@ const uploadGuestList = async (req, res) => {
     where: { eventId: event.id },
   });
 
-  if (holders.length !== guestList.length) {
-    const guestListFormatted = guestList.filter((guest) => {
-      const ind = holders.findIndex((holder) => guest[0] === holder.ticketId);
-      log("app", "uploadFile", { guest, ind: !!ind });
-      return !!ind;
-    });
-    log("App", "uploadfile", { guestList, guestListFormatted, holders });
+  if (holders.length < guestList.length) {
+    try {
+      const guestListFormatted = guestList.filter((guest) => {
+        const ticketID = guest[0];
+        const ind = holders.findIndex((holder) => ticketID === holder.ticketId);
 
-    const guests = guestListFormatted.map((guest) => {
-      const guestNames = guest[1].split(" ");
+        log("app", "uploadFile", { guest, ind: ind });
+        return ind < 0;
+      });
+      log("App", "uploadfile", { guestList, guestListFormatted, holders });
 
-      return {
-        firstName: guestNames[0],
-        lastName: guestNames[guestNames.length - 1],
-        emailId: guest[2],
-        eventName: event.eventname,
-        ticketId: guest[0],
-      };
-    });
-    if (guestListFormatted.length) {
-      const web3response = await writeHoldersToBatch(
-        guests,
-        event.contractAddress
-      );
+      const guests = guestListFormatted.map((guest) => {
+        const guestNames = guest[1].split(" ");
 
-      if (web3response.success) {
-        console.log({ web3response, guests });
-        const inputParams = guests.map((guest) => {
-          const { firstName, lastName, emailId, ticketId } = guest;
-
-          return {
-            firstName,
-            lastName,
-            emailId,
-            firstAllowedEntryDate: event.firstAllowedEntryDate,
-            lastAllowedEntryDate: event.lastAllowedEntryDate,
-            ticketId,
-          };
-        });
-        addBatch({
-          event,
-          batchId: web3response.batchId,
-          addBatchTimestamp: Date.now(),
-          contractAddress: event.contractAddress,
-          inputParams,
-        });
-        res.json({
-          message: `${guestListFormatted.length} guests added and sent email!`,
+        return {
+          firstName: guestNames[0],
+          lastName: guestNames[guestNames.length - 1],
+          emailId: guest[2],
+          eventName: event.eventname,
+          ticketId: guest[0],
+        };
+      });
+      if (guestListFormatted.length) {
+        const web3response = await writeHoldersToBatch(
           guests,
-        });
+          event.contractAddress
+        );
+
+        if (web3response.success) {
+          console.log({ web3response, guests });
+          const inputParams = guests.map((guest) => {
+            const { firstName, lastName, emailId, ticketId } = guest;
+
+            return {
+              firstName,
+              lastName,
+              emailId,
+              firstAllowedEntryDate: event.firstAllowedEntryDate,
+              lastAllowedEntryDate: event.lastAllowedEntryDate,
+              ticketId,
+            };
+          });
+          addBatch({
+            event,
+            batchId: web3response.batchId,
+            addBatchTimestamp: Date.now(),
+            contractAddress: event.contractAddress,
+            inputParams,
+          });
+          res.json({
+            message: `${guestListFormatted.length} guests added and sent email!`,
+            guests,
+          });
+        } else {
+          res.sendStatus(500).json({
+            message: "Error... Something Went Wrong in contract Interaction",
+            error: web3response.message,
+          });
+        }
       } else {
-        res.sendStatus(500).json({
-          message: "Error... Something Went Wrong in contract Interaction",
-          error: web3response.message,
-        });
+        res.sendStatus(500).json({ message: "Error... Something Went Wrong" });
       }
-    } else {
-      res.sendStatus(500).json({ message: "Error... Something Went Wrong" });
+    } catch (err) {
+      logError("uploadFile", "body", err);
     }
   } else
     res.send({
